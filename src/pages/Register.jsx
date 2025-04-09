19fd07409f598699487d9fec066891b65a3e81ed
quang khanh <nidakhanh0@gmail.com>
2025-04-09 21:48:37 +0700

    sửa tên

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -52,7 +52,7 @@ const Login = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <div className="w-full max-w-md p-8 bg-white shadow-xl rounded-lg h-auto min-h-[450px]">
-        <h2 className="text-2xl font-semibold text-center text-gray-700 mb-6">Đăng Nhập</h2>
+        <h2 className="text-2xl font-semibold text-center text-gray-700 mb-6">Đăng Ký</h2>
         <form onSubmit={handleRegister}>
           <div className="mb-4">
             <label className="block text-gray-600 text-sm font-medium mb-1">Email</label>
